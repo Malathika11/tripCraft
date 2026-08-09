c4724299892ef662ef016a93ae4bd2c0fc7a023b
Malathika11 <gmmalathika99@gmail.com>
2026-08-09 10:34:56 +0530

    Added Aiven SSL support

--- backend/db.js
+++ backend/db.js
@@ -3,16 +3,20 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("✅ MySQL Connected");
   }
 });
 
